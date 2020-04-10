76ce5856c079bb39134e7a252f9c6236db5ca807
Olivia Briggs <ofbriggs@gmail.com>
2020-04-10 13:30:21 -0400

    Update config

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-const { version } = require("./package.json");
-const readline = require("readline");
-import { validate, Type } from "./instr";
+const { version } = require('./package.json');
+const readline = require('readline');
+import { validate, Type } from './instr';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: ">> ",
+  prompt: '>> ',
 });
 
 console.log(`Plavit Version: ${version}`);
 
-let expression = "";
+let expression = '';
 rl.prompt();
-rl.on("line", (line) => {
-  const endProgram = line.trim().slice(-1)[0] === ";";
+rl.on('line', (line: string) => {
+  const endProgram = line.trim().slice(-1)[0] === ';';
   try {
     if (endProgram) {
       expression += line.trimEnd().slice(0, -1);
-      console.log("Validation: ", validate(JSON.parse(expression)));
-      expression = "";
-      rl.setPrompt(">> ");
+      console.log('Validation: ', validate(JSON.parse(expression)));
+      expression = '';
+      rl.setPrompt('>> ');
       rl.prompt();
     } else {
-      rl.setPrompt("");
+      rl.setPrompt('');
       expression += line;
     }
   } catch (e) {
     console.error(e);
     rl.close();
   }
-}).on("close", () => {
+}).on('close', () => {
   process.exit();
 });
--- src/instr.ts
+++ src/instr.ts
@@ -1,90 +1,90 @@
 export enum Type {
-  F = "function",
-  N = "number",
-  B = "boolean",
-  S = "string",
-  T = "tuple",
-  L = "list"
+  F = 'function',
+  N = 'number',
+  B = 'boolean',
+  S = 'string',
+  T = 'tuple',
+  L = 'list',
 }
 
 export enum CompoundType {
-    T = "tuple",
-    L = "list"
+  T = 'tuple',
+  L = 'list',
 }
 
-function isExpr(expr) {
-    return Array.isArray(expr) && isOperator(expr[0]) && !isDefinition(expr[0]);
+function isExpr(expr: unknown) {
+  return Array.isArray(expr) && isOperator(expr[0]) && !isDefinition(expr[0]);
 }
 
-function isDefinition(expr) {
-    if (Array.isArray(expr) && !isOperator(expr[0])) {
-        switch (expr[0]) {
-            case '@t': {
-                return CompoundType.T;
-            }
-            case '@l': {
-                // list must have homogenous types for its elements
-                return validate(expr.slice(1), getType(expr[1]));
-            }
-            default: {
-                return false;
-            }
-        }
+function isDefinition(expr: unknown) {
+  if (Array.isArray(expr) && !isOperator(expr[0])) {
+    switch (expr[0]) {
+      case '@t': {
+        return CompoundType.T;
+      }
+      case '@l': {
+        // list must have homogenous types for its elements
+        return validate(expr.slice(1), getType(expr[1]));
+      }
+      default: {
+        return false;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function isOperator(op) {
-    return operators.hasOwnProperty(op);
+  return operators.hasOwnProperty(op);
 }
 
-    function getType(expr) {
-        return isExpr(expr) ? operators[expr[0]] : isDefinition(expr) ? expr[0] === '@t' ? [expr[0], ...expr.slice(1).map(getType)] : [expr[0], getType(expr[1])] : typeof expr;
-    }
+function getType(expr) {
+  return isExpr(expr)
+    ? operators[expr[0]]
+    : isDefinition(expr)
+    ? expr[0] === '@t'
+      ? [expr[0], ...expr.slice(1).map(getType)]
+      : [expr[0], getType(expr[1])]
+    : typeof expr;
+}
 
 const operators = {
   // Numerical Operators
-  ":+": [Type.N, Type.N, Type.N],
-  ":-": [Type.N, Type.N, Type.N],
-  ":*": [Type.N, Type.N, Type.N],
-  ":/": [Type.N, Type.N, Type.N],
-  ":<": [Type.N, Type.N, Type.B],
-  ":>": [Type.N, Type.N, Type.B],
-  ":<=": [Type.N, Type.N, Type.B],
-  ":>=": [Type.N, Type.N, Type.B],
-  ":=n": [Type.N, Type.N, Type.B],
-  ":=s": [Type.S, Type.S, Type.B],
+  ':+': [Type.N, Type.N, Type.N],
+  ':-': [Type.N, Type.N, Type.N],
+  ':*': [Type.N, Type.N, Type.N],
+  ':/': [Type.N, Type.N, Type.N],
+  ':<': [Type.N, Type.N, Type.B],
+  ':>': [Type.N, Type.N, Type.B],
+  ':<=': [Type.N, Type.N, Type.B],
+  ':>=': [Type.N, Type.N, Type.B],
+  ':=n': [Type.N, Type.N, Type.B],
+  ':=s': [Type.S, Type.S, Type.B],
   // Boolean Operators
-  ":||": [Type.B, Type.B, Type.B],
-  ":&&": [Type.B, Type.B, Type.B],
+  ':||': [Type.B, Type.B, Type.B],
+  ':&&': [Type.B, Type.B, Type.B],
   // Lookup Operators
-  ":.": [Type.N, Type.N],
+  ':.': [Type.N, Type.N],
   // cast to string
-  ":s": [Type.N, Type.S],
+  ':s': [Type.N, Type.S],
   // Conditional Operators
-  ":?n": [Type.B, Type.N, Type.N, Type.N],
-  ":?s": [Type.B, Type.S, Type.S, Type.S],
-  ":?b": [Type.B, Type.B, Type.B, Type.B],
-  ":?f": [Type.B, Type.F, Type.F, Type.F],
+  ':?n': [Type.B, Type.N, Type.N, Type.N],
+  ':?s': [Type.B, Type.S, Type.S, Type.S],
+  ':?b': [Type.B, Type.B, Type.B, Type.B],
+  ':?f': [Type.B, Type.F, Type.F, Type.F],
 };
-
-export function validate(expr: unknown, type?: Type) {
+type Operator = keyof typeof operators;
+export function validate(expr: unknown, type?: Type): boolean {
   // Validate operator, arguments and return type
   if (Array.isArray(expr)) {
     try {
       const [op, ...cdr] = expr;
       console.log(`Checking if (${op}) exists:`, operators.hasOwnProperty(op));
-      if (
-        operators.hasOwnProperty(op) &&
-        cdr.length === operators[op].length - 1
-      ) {
-        const retType = operators[op].slice(-1)[0];
-        return (
-          (!type || retType === type) &&
-          cdr.every((item, idx) => validate(item, operators[op][idx]))
-        );
+      if (operators.hasOwnProperty(op as Operator) && cdr.length === operators[op as Operator].length - 1) {
+        const retType = operators[op as Operator].slice(-1)[0];
+        return (!type || retType === type) && cdr.every((item, idx) => validate(item, operators[op as Operator][idx]));
       }
-      console.error("unhandled operator referenced:", op);
+      console.error('unhandled operator referenced:', op);
       return false;
     } catch (e) {
       console.error(e);
@@ -93,11 +93,9 @@ export function validate(expr: unknown, type?: Type) {
   }
 
   const typeComparison =
-    type === undefined
-      ? [Type.F, Type.N, Type.B, Type.S].includes(typeof expr as Type)
-      : typeof expr === type;
+    type === undefined ? [Type.F, Type.N, Type.B, Type.S].includes(typeof expr as Type) : typeof expr === type;
   if (!typeComparison) {
-    console.error(expr, "is not of type:", type);
+    console.error(expr, 'is not of type:', type);
   }
   return typeComparison;
 }
