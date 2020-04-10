1044a4bc0695820511d1d4ea2fcabc503d30e3bc
Olivia Briggs <ofbriggs@gmail.com>
2020-04-10 00:43:32 -0400

    Add mini-validator

---
+++ index.ts
@@ -0,0 +1,33 @@
+const { version } = require("./package.json");
+const readline = require("readline");
+import { validate, Type } from "./instr";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: ">> ",
+});
+
+console.log(`Plavit Version: ${version}`);
+
+let expression = "";
+rl.prompt();
+rl.on("line", (line) => {
+  const endProgram = line.trim().slice(-1)[0] === ";";
+  try {
+    if (endProgram) {
+      expression += line.trimEnd().slice(0, -1);
+      console.log("Validation: ", validate(JSON.parse(expression)));
+      expression = "";
+      rl.setPrompt(">> ");
+      rl.prompt();
+    } else {
+      rl.setPrompt("");
+      expression += line;
+    }
+  } catch (e) {
+    console.error(e);
+    rl.close();
+  }
+}).on("close", () => {
+  process.exit();
+});
--- instr.ts
+++ instr.ts
@@ -1,19 +1,66 @@
-enum Type {
-  F = "f",
-  N = "n",
-  B = "b",
-  S = "s"
+export enum Type {
+  F = "function",
+  N = "number",
+  B = "boolean",
+  S = "string",
 }
 
 const operators = {
+  // Numerical Operators
   ":+": [Type.N, Type.N, Type.N],
   ":-": [Type.N, Type.N, Type.N],
   ":*": [Type.N, Type.N, Type.N],
   ":/": [Type.N, Type.N, Type.N],
+  ":<": [Type.N, Type.N, Type.B],
+  ":>": [Type.N, Type.N, Type.B],
+  ":<=": [Type.N, Type.N, Type.B],
+  ":>=": [Type.N, Type.N, Type.B],
+  ":=n": [Type.N, Type.N, Type.B],
+  ":=s": [Type.S, Type.S, Type.B],
+  // Boolean Operators
+  ":||": [Type.B, Type.B, Type.B],
+  ":&&": [Type.B, Type.B, Type.B],
+  // Lookup Operators
   ":.": [Type.N, Type.N],
+  // cast to string
   ":s": [Type.N, Type.S],
+  // Conditional Operators
   ":?n": [Type.B, Type.N, Type.N, Type.N],
   ":?s": [Type.B, Type.S, Type.S, Type.S],
   ":?b": [Type.B, Type.B, Type.B, Type.B],
-  ":?f": [Type.B, Type.F, Type.F, Type.F]
+  ":?f": [Type.B, Type.F, Type.F, Type.F],
 };
+
+export function validate(expr: unknown, type?: Type) {
+  // Validate operator, arguments and return type
+  if (Array.isArray(expr)) {
+    try {
+      const [op, ...cdr] = expr;
+      console.log(`Checking if (${op}) exists:`, operators.hasOwnProperty(op));
+      if (
+        operators.hasOwnProperty(op) &&
+        cdr.length === operators[op].length - 1
+      ) {
+        const retType = operators[op].slice(-1)[0];
+        return (
+          (!type || retType === type) &&
+          cdr.every((item, idx) => validate(item, operators[op][idx]))
+        );
+      }
+      console.error("unhandled operator referenced:", op);
+      return false;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  const typeComparison =
+    type === undefined
+      ? [Type.F, Type.N, Type.B, Type.S].includes(typeof expr as Type)
+      : typeof expr === type;
+  if (!typeComparison) {
+    console.error(expr, "is not of type:", type);
+  }
+  return typeComparison;
+}
